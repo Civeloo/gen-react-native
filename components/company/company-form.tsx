91dcb74bb0c199ae26f4115d33606532fe67f1d3
DeGsoft <diegoezequielguillen@gmail.com>
2025-08-02 13:14:45 -0300

    chore (company-form): add concept and ptoVta

--- components/company/company-form.tsx
+++ components/company/company-form.tsx
@@ -1,26 +1,54 @@
 import {getLocalizedText} from '@/languages/languages';
-import {yupResolver} from "@hookform/resolvers/yup";
+import {yupResolver} from '@hookform/resolvers/yup';
 import React from 'react';
-import {FormProvider, SubmitHandler, useForm, Controller} from 'react-hook-form';
+import {Controller, FormProvider, SubmitHandler, useForm} from 'react-hook-form';
 import {Button, Platform, StyleSheet, Text, View} from 'react-native';
-import * as yup from "yup";
+import * as yup from 'yup';
 import {TextInputController} from '../text-input-controller';
-import {Company} from "@/types/types";
-import {DropDownPicker} from "@/components/drop-down-picker";
+import {Company} from '@/types/types';
+import {DropDownPicker} from '@/components/drop-down-picker';
 import COUNTRIES from '@/languages/countries.json';
+
+const CONCEPTS = [
+    {
+        'label': 'Productos',
+        'value': '1'
+    },
+    {
+        'label': 'Servicios',
+        'value': '2'
+    },
+    {
+        'label': 'Productos y  Servicios',
+        'value': '3'
+    },
+];
+const getPtoVtas = () => {
+    const x = [];
+    for (let i = 1; i < 10; i++) {
+        const nro = String(i);
+        x.push({
+            'label': nro,
+            'value': nro
+        });
+    }
+    return x;
+};
 
 const schema = yup
     .object({
         companyName: yup.string().max(50).required(),
-        contact: yup.string().max(50),
-        address: yup.string().max(100),
-        city: yup.string().max(50),
-        state: yup.string().max(50),
-        postalCode: yup.string().max(50),
-        country: yup.string().max(50),
-        phone: yup.string().max(50),
-        tin: yup.string().max(50),
-        type: yup.string().max(50)
+        contact: yup.string().max(50).defined(),
+        address: yup.string().max(100).defined(),
+        city: yup.string().max(50).defined(),
+        state: yup.string().max(50).defined(),
+        postalCode: yup.string().max(50).defined(),
+        country: yup.string().max(50).defined(),
+        concept: yup.string().max(50).defined(),
+        phone: yup.string().max(50).defined(),
+        tin: yup.string().max(50).defined(),
+        type: yup.string().max(50).defined(),
+        ptoVta: yup.string().max(50).defined(),
     })
     .required();
 
@@ -42,17 +70,20 @@ export const CompanyForm: React.FC<CompanyFormProps> = ({company, onSave}) => {
             state: company?.state || '',
             postalCode: company?.postalCode || '',
             country: company?.country || '',
+            concept: company?.concept || '3',
             phone: company?.phone || '',
             tin: company?.tin || '',
             type: company?.type || '',
+            ptoVta: company?.ptoVta || '',
         }
     });
+
+    const country = methods.watch('country');
 
     const onSubmit: SubmitHandler<FormValues> = (values) => {
         values.type = values?.type?.toUpperCase() || 'USA';
         const companyValues = values as Company;
         onSave(companyValues, company?.id);
-        // methods.reset();
     };
 
     const FormContent = (
@@ -60,47 +91,47 @@ export const CompanyForm: React.FC<CompanyFormProps> = ({company, onSave}) => {
             <View style={styles.input}>
                 <Text style={styles.label}>✏</Text>
                 <TextInputController
-                    name="companyName"
+                    name='companyName'
                     placeholder={getLocalizedText('company_name_placeholder')}
-                    keyboardType="default"
+                    keyboardType='default'
                 /></View>
             <View style={styles.input}>
                 <Text style={styles.label}>🤵</Text>
                 <TextInputController
-                    name="contact"
+                    name='contact'
                     placeholder={getLocalizedText('contact_placeholder')}
-                    keyboardType="default"
+                    keyboardType='default'
                 /></View>
             <View style={styles.input}>
                 <Text style={styles.label}>📍</Text>
                 <TextInputController
-                    name="address"
+                    name='address'
                     placeholder={getLocalizedText('address_placeholder')}
                 /></View>
             <View style={styles.input}>
                 <Text style={styles.label}>🏙</Text>
                 <TextInputController
-                    name="city"
+                    name='city'
                     placeholder={getLocalizedText('city_placeholder')}
                 /></View>
             <View style={styles.input}>
                 <Text style={styles.label}>🏞</Text>
                 <TextInputController
-                    name="state"
+                    name='state'
                     placeholder={getLocalizedText('state_placeholder')}
                 /></View>
             <View style={styles.input}>
                 <Text style={styles.label}>📮</Text>
                 <TextInputController
-                    name="postalCode"
+                    name='postalCode'
                     placeholder={getLocalizedText('postal_code_placeholder')}
                 /></View>
             <View style={styles.dropDownPicker}>
                 <Text style={styles.label}>🌎</Text>
                 <Controller
                     control={methods.control}
-                    rules={{ required: true }}
-                    render={({ field: { onChange, value } }) => (
+                    rules={{required: true}}
+                    render={({field: {onChange, value}}) => (
                         <DropDownPicker
                             data={COUNTRIES}
                             placeholder={getLocalizedText('country_placeholder')}
@@ -108,30 +139,63 @@ export const CompanyForm: React.FC<CompanyFormProps> = ({company, onSave}) => {
                             value={value}
                         />
                     )}
-                    name="country"
+                    name='country'
                 />
             </View>
             <View style={styles.input}>
                 <Text style={styles.label}>📞</Text>
                 <TextInputController
-                    name="phone"
+                    name='phone'
                     placeholder={getLocalizedText('phone_placeholder')}
-                    keyboardType="default"
+                    keyboardType='default'
                 /></View>
             <View style={styles.input}>
                 <Text style={styles.label}>🎫</Text>
                 <TextInputController
-                    name="tin"
+                    name='tin'
                     placeholder={getLocalizedText('tin_placeholder')}
-                    keyboardType="default"
+                    keyboardType='default'
                 /></View>
             <View style={styles.input}>
                 <Text style={styles.label}>🏢</Text>
                 <TextInputController
-                    name="type"
+                    name='type'
                     placeholder={getLocalizedText('type_placeholder')}
                 /></View>
-
+            {country === 'AR' && <>
+                <View style={styles.dropDownPicker}>
+                    <Text style={styles.label}>🧾</Text>
+                    <Controller
+                        control={methods.control}
+                        rules={{required: true}}
+                        render={({field: {onChange, value}}) => (
+                            <DropDownPicker
+                                data={CONCEPTS}
+                                placeholder={getLocalizedText('concept_placeholder')}
+                                onChange={onChange}
+                                value={value}
+                            />
+                        )}
+                        name='concept'
+                    />
+                </View>
+                <View style={styles.dropDownPicker}>
+                    <Text style={styles.label}>🏪</Text>
+                    <Controller
+                        control={methods.control}
+                        rules={{required: true}}
+                        render={({field: {onChange, value}}) => (
+                            <DropDownPicker
+                                data={getPtoVtas()}
+                                placeholder={getLocalizedText('pto_vta_placeholder')}
+                                onChange={onChange}
+                                value={value}
+                            />
+                        )}
+                        name='ptoVta'
+                    />
+                </View>
+            </>}
             <View style={styles.okButton}>
                 <Button title={getLocalizedText('ok')} onPress={methods.handleSubmit(onSubmit)}/>
             </View>
@@ -169,8 +233,8 @@ const styles = StyleSheet.create({
         fontSize: 28,
         fontWeight: 'bold',
         width: 40,
-        textAlign: "center",
-        // alignSelf: "center",
+        textAlign: 'center',
+        // alignSelf: 'center',
         // backgroundColor: 'yellow'
     },
     dropDownPicker: {
